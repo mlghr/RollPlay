0598e54ec8345d78c03f136d3cc9f9c0747b742e
mlghr <michael.rosenbaum1@gmail.com>
2020-12-20 01:14:10 -0800

    can use match button to request new user (useEffect update)

--- frontend/src/Card.js
+++ frontend/src/Card.js
@@ -4,16 +4,31 @@ import "./Card.css";
 
 function Card ({username, src, first, last, city, country, age}) {
     return (
-        <div className="container">
-            <div className="card-body">
-                <div className="card-head">{username}</div>
-                <img className="card-src" src={src} alt={first} />
-                <div className="card-text">Name: {first} {last}</div>
-                <div className="card-text">City: {city}</div>
-                <div className="card-text">Country: {country}</div>
-                <div className="card-text">Age: {age}</div>
+        // <div className="container">
+        //     <div className="card-body">
+        //         <div className="card-head">{username}</div>
+        //         <img className="card-src" src={src} alt="" />
+        //         <div className="card-text">Name: {first} {last}</div>
+        //         <div className="card-text">City: {city}</div>
+        //         <div className="card-text">Country: {country}</div>
+        //         <div className="card-text">Age: {age}</div>
+        //     </div>
+        // </div>
+    <>
+        <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img className="flip-image" src={src} alt="" />
+              </div>
+              <div className="flip-card-back">
+                <h1>{first} {last}</h1>
+                <p>{username}</p>
+                <p>City: {city} & Country: {country}</p>
+                <p>Age: {age}</p>
+              </div>
             </div>
         </div>
+    </>
     );
 }
 
--- frontend/src/Carousel.js
+++ frontend/src/Carousel.js
@@ -9,45 +9,48 @@ function Carousel() {
   const [isLoading, setIsLoading] = useState(false);
   const [user, setUser] = useState([]);
 
+  async function getRandomUser() {
+    try {
+      setIsLoading(true);
+      //queries randomuser.me for random person 
+      let randomMeRes = await axios.get(`https://randomuser.me/api/?inc=name,location,dob,login,picture&results=1`)
+      let randomUser = randomMeRes.data.results[0];
+
+      setUser([]);
+
+      setUser(d => [
+        ...d,
+        {
+          id: randomUser.login.id,
+          username: randomUser.login.username,
+          first: randomUser.name.first,
+          last: randomUser.name.last,
+          city: randomUser.location.city,
+          country: randomUser.location.country,
+          age: randomUser.dob.age,
+          src: randomUser.picture.large
+        }
+      ]);
+      setIsLoading(false);
+    } catch(err){
+      alert(err);
+    }
+  }
+  
+
   useEffect(() => {
-    
-    async function getRandomUser() {
-      try {
-        //generates a random id to query db for next possible match  OLD CODE, IGNORE
+        //OLD CODE, IGNORE
+        //generates a random id to query db for next possible match  
         // let userRes = await axios.get(`${DB_API}users/match`);
         // if(!userRes){
         //   throw new Error("User does not exist")
         // }
         // let randomUser = userRes.data.user;
-
-        setIsLoading(true);
-        //queries randomuser.me for random person 
-        let randomMeRes = await axios.get(`https://randomuser.me/api/?inc=name,location,dob,login,picture&results=1`)
-        let randomUser = randomMeRes.data.results[0];
-
-        setUser(d => [
-          ...d,
-          {
-            id: randomUser.login.id,
-            username: randomUser.login.username,
-            first: randomUser.name.first,
-            last: randomUser.name.last,
-            city: randomUser.location.city,
-            country: randomUser.location.country,
-            age: randomUser.dob.age,
-            src: randomUser.picture.large
-          }
-        ]);
-        setIsLoading(false);
-      } catch(err){
-        alert(err);
-      }
-    }
-    getRandomUser();
+     getRandomUser();
   }, [setUser]);
 
   const skipUser = () => console.log("this is skipUser function");
-  const confirmMatch = () => setUser([user]);
+  const confirmMatch = () => getRandomUser();
 
   const userToDisplay = user.map(u => (
     <Card key={u.id} 
@@ -62,12 +65,12 @@ function Carousel() {
   
     return (
       <>
-        {isLoading ? <h1>Loading data...</h1> : 
+        {isLoading ? <div className="loader"></div> : 
         <div>
-          User info: {userToDisplay}
+        {userToDisplay}
           <div>
             <img src={user.src}/>
-            <button className="genBtn" onClick={skipUser}>Skip</button>
+            <button className="genBtn" display="hidden" onClick={skipUser}>Skip</button>
             <button className="genBtn" onClick={confirmMatch}>Match</button> 
           </div>
         </div>
