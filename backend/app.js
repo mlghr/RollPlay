742ccbbbf227ad85c9d3daf48b04ec9809b53d7c
mlghr <michael.rosenbaum1@gmail.com>
2021-01-05 16:48:41 -0800

    socket code in app.js

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,52 @@ app.use("/users", userRoutes);
 app.use("/characters", characterRoutes);
 app.use("/evaluations", evaluationRoutes);
 
+const http = require('http');
+const socketio = require('socket.io');
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
+io.on('connect', (socket) => {
+  console.log("A user has connected");
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if(error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    }
+  })
+});
+
+server.listen(process.env.PORT || 6000, () => console.log(`Server has started on port 6000.`));
+
 app.use(function (err, req, res, next) {
   const status = err.status || 500;
   const message = err.message;
